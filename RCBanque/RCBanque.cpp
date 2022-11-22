b9957e7689f525eea374a2698a341ebc76865a59
Paulbarge <90193040+Paulbarge@users.noreply.github.com>
2022-11-22 14:18:17 +0100

    Ajoutez des fichiers projet.

---
+++ RCBanque/RCBanque.cpp
@@ -0,0 +1,12 @@
+﻿// RCBanque.cpp : définit le point d'entrée de l'application.
+//
+
+#include "RCBanque.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
