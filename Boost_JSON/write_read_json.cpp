393e0c8e3f203ec0e9805e126e0ac68fbddd52df
Paulbarge <90193040+Paulbarge@users.noreply.github.com>
2022-12-30 01:03:02 +0100

    fpaul

--- Boost_JSON/write_read_json.cpp
+++ Boost_JSON/write_read_json.cpp
@@ -2,6 +2,8 @@
 #include <boost/property_tree/ptree.hpp>
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include <cstring> 
 
 
 #ifdef _WIN32
@@ -27,8 +29,8 @@ ptree get_a_ptree_from_a_customer(client &customer) {
     pt.put("Nom", customer.nomGet());
     pt.put("Prenom", customer.prenomGet());
     pt.put("dateNaissance", customer.dateNaissanceGet());
-    pt.put("numeroAdresse", customer.numeroAdresseGet());
-    pt.put("adresse", customer.adresseGet());
+   // pt.put("numeroAdresse", customer.numeroAdresseGet());
+   // pt.put("adresse", customer.adresseGet());
     pt.put("tel", customer.telGet());
     pt.put("compte_courant", customer.comptecourantGet());
     pt.put("compte_epargne", customer.compteepargneGet());
@@ -45,8 +47,8 @@ client get_a_customer_from_a_ptree(ptree& pt) {
     string Prenom = pt.get<string>("Prenom");
     int dateNaissance = pt.get<int>("dateNaissance");
 
-    int numeroAdresse = pt.get<int>("numeroAdresse");
-    string adresse = pt.get<string>("adresse"); 
+    //int numeroAdresse = pt.get<int>("numeroAdresse");
+    //string adresse = pt.get<string>("adresse"); 
     int tel = pt.get<int>("tel", 0);
 
     int compte_courant = pt.get<int>("compte_courant", 0);
@@ -55,40 +57,224 @@ client get_a_customer_from_a_ptree(ptree& pt) {
 
 
 
-    client customer(numero_client,Nom,Prenom,dateNaissance,numeroAdresse,adresse,tel,compte_courant,compte_epargne,interet);
+    client customer(numero_client,Nom,Prenom,dateNaissance,tel,compte_courant,compte_epargne,interet);
 
     return customer;
 }
+
+void add_customer1(client& customer) {
+    ptree pt_in;
+    ptree pt_accounts;
+    ptree pt_write;
+    //on recuper les client deja present dans le json 
+    ifstream file_in("C:/Users/Administrateur/source/repos/RCBanque/Boost_JSON/bdd1.json");
+    read_json(file_in, pt_in);
+    file_in.close();
+    // on les ajoute tous a un ptree
+    for (ptree::value_type& customer : pt_in.get_child("Customers")) {
+        auto custom = get_a_customer_from_a_ptree(customer.second);
+        pt_accounts.push_back({ "", get_a_ptree_from_a_customer(custom) });
+        
+    }
+    // on ajoute le nouveau client au ptree
+    pt_accounts.push_back({ "", get_a_ptree_from_a_customer(customer) });
+   
+
+    pt_write.add_child("Customers", pt_accounts);
+
+   
+    // on ecrit dans le json
+    ofstream file_out("C:/Users/Administrateur/source/repos/RCBanque/Boost_JSON/bdd1.json");
+    write_json(file_out, pt_write);
+    file_out.close();
+}
+void add_customer2(client& customer) {
+    ptree pt_in;
+    ptree pt_accounts;
+    ptree pt_write;
+    //on recuper les client deja present dans le json 
+    ifstream file_in("C:/Users/Administrateur/source/repos/RCBanque/Boost_JSON/bdd2.json");
+    read_json(file_in, pt_in);
+    file_in.close();
+    // on les ajoute tous a un ptree
+    for (ptree::value_type& customer : pt_in.get_child("Customers")) {
+        auto custom = get_a_customer_from_a_ptree(customer.second);
+        pt_accounts.push_back({ "", get_a_ptree_from_a_customer(custom) });
+
+    }
+    // on ajoute le nouveau client au ptree
+    pt_accounts.push_back({ "", get_a_ptree_from_a_customer(customer) });
+
+
+    pt_write.add_child("Customers", pt_accounts);
+
+
+    // on ecrit dans le json
+    ofstream file_out("C:/Users/Administrateur/source/repos/RCBanque/Boost_JSON/bdd2.json");
+    write_json(file_out, pt_write);
+    file_out.close();
+}
+void add_customer3(client& customer) {
+    ptree pt_in;
+    ptree pt_accounts;
+    ptree pt_write;
+    //on recuper les client deja present dans le json 
+    ifstream file_in("C:/Users/Administrateur/source/repos/RCBanque/Boost_JSON/bdd3.json");
+    read_json(file_in, pt_in);
+    file_in.close();
+    // on les ajoute tous a un ptree
+    for (ptree::value_type& customer : pt_in.get_child("Customers")) {
+        auto custom = get_a_customer_from_a_ptree(customer.second);
+        pt_accounts.push_back({ "", get_a_ptree_from_a_customer(custom) });
+
+    }
+    // on ajoute le nouveau client au ptree
+    pt_accounts.push_back({ "", get_a_ptree_from_a_customer(customer) });
+
+
+    pt_write.add_child("Customers", pt_accounts);
+
+
+    // on ecrit dans le json
+    ofstream file_out("C:/Users/Administrateur/source/repos/RCBanque/Boost_JSON/bdd3.json");
+    write_json(file_out, pt_write);
+    file_out.close();
+}
+
+vector<client> json_to_vector1() {
+    std::vector<client> cust;
+    ptree pt_in;
+
+    //on recuper les client deja present dans le json 
+    ifstream file_in("C:/Users/Administrateur/source/repos/RCBanque/Boost_JSON/bdd1.json");
+    read_json(file_in, pt_in);
+    file_in.close();
+
+    // on les ajoute tous a un ptree
+    for (ptree::value_type& customer : pt_in.get_child("Customers")) {
+        auto customer_f_ptree = get_a_customer_from_a_ptree(customer.second);
+        cust.push_back(customer_f_ptree);
+
+    }
+    return cust;
+}
+vector<client> json_to_vector2() {
+    std::vector<client> cust;
+    ptree pt_in;
+
+    //on recuper les client deja present dans le json 
+    ifstream file_in("C:/Users/Administrateur/source/repos/RCBanque/Boost_JSON/bdd2.json");
+    read_json(file_in, pt_in);
+    file_in.close();
+
+    // on les ajoute tous a un ptree
+    for (ptree::value_type& customer : pt_in.get_child("Customers")) {
+        auto customer_f_ptree = get_a_customer_from_a_ptree(customer.second);
+        cust.push_back(customer_f_ptree);
+
+    }
+    return cust;
+}
+vector<client> json_to_vector3() {
+    std::vector<client> cust;
+    ptree pt_in;
+
+    //on recuper les client deja present dans le json 
+    ifstream file_in("C:/Users/Administrateur/source/repos/RCBanque/Boost_JSON/bdd3.json");
+    read_json(file_in, pt_in);
+    file_in.close();
+
+    // on les ajoute tous a un ptree
+    for (ptree::value_type& customer : pt_in.get_child("Customers")) {
+        auto customer_f_ptree = get_a_customer_from_a_ptree(customer.second);
+        cust.push_back(customer_f_ptree);
+
+    }
+    return cust;
+}
+
+
+int numagence(string enter) {
+    string delimiter = ",";
+    string token = enter.substr(0, enter.find(delimiter)); 
+    int sortie = atoi(token.c_str());
+    sortie = sortie % 1000;
+    return sortie;
+}
+
+int numclient(string enter) {
+    string delimiter = ",";
+    string token = enter.substr(0, enter.find(delimiter));
+    int sortie = atoi(token.c_str());
+    return sortie;
+}
+
+int sommevirement(string enter) {
+    string delimiter = ",";
+    string token = enter.substr(enter.find(delimiter)+1,enter.length());
+    int sortie = atoi(token.c_str());
+    return sortie;
+}
+
+
+client virement(client customer, string enter) {
+    int vir = sommevirement(enter);
+    int solde = customer.comptecourantGet();
+    int total = solde + vir;
+    customer.comptecourantSet(total);
+    return customer;
+}
+
+ 
+
+
 
 
 int main(int argc, char** argv) {
     ptree pt_write;
     ptree pt_accounts;
+    ptree pt_test;
 
     try {
-        client customer1(1001, "Barge","Paul",13092003,2,"alle du s",0652270107,200,300,3);
+        client customer1(1003, "bonjour","bochu",13092003,0652270107,200,300,3);
+        
+        
 
-        client customer2(1002, "simon", "bernard", 12042001,3,"rue vauband",0624522701, 1200, 300, 5);
- 
+        string a = "2003,211";
 
-        pt_accounts.push_back({ "", get_a_ptree_from_a_customer(customer1) });
-        pt_accounts.push_back({ "", get_a_ptree_from_a_customer(customer2) });
-        pt_write.add_child("Customers", pt_accounts);
+        virement(customer1, a);
 
-        ofstream file_out("bdd.json");
-        write_json(file_out, pt_write);
-        file_out.close();
+        cout << customer1 << endl;
+        
 
-        ifstream file_in("bdd.json");
-        read_json(file_in, pt_write);
-        file_in.close();
 
-        for (ptree::value_type& customer : pt_write.get_child("Customers")) {
-            auto custom = get_a_customer_from_a_ptree(customer.second);
 
-             cout << custom << std::endl;
+        /*
+        add_customer2(customer1);
+        add_customer3(customer1);
+
+        
+        vector<client> ca = json_to_vector2();
+        vector<client> cb = json_to_vector3();
+
+       
+        for (int i = 0; i < 2; i++) {
+            cout << ca[i] << endl;
         }
+        for (int i = 0; i < 2; i++) {
+            cout << cb[i] << endl;
+        }*/
+
+        int n = numagence(a);
+        int b = numclient(a);
+        int w = sommevirement(a);
+        cout << n << std::endl;
+        cout << b << std::endl;
+        cout << w << std::endl;
+
+
     }
+
     catch (std::exception& e) {
         // Other errors
     }
--- RCBanque/clients.hpp
+++ RCBanque/clients.hpp
@@ -13,8 +13,8 @@ private:
     string nom;
     string prenom;
     int dateNaissance;
-    int numeroAdresse;
-    string adresse;
+    //int numeroAdresse;
+    //string adresse;
     int tel;
     // a l'inscription on choisie la somme de chaque compte
     int compte_courant;
@@ -23,15 +23,15 @@ private:
 
 public:
     // constructeur
-    client(int, string, string, int, int, string, int, int, int, int);
+    client(int, string, string, int, int, int, int, int);
 
     // getter
     int numeroClientGet();
     string nomGet();
     string prenomGet();
     int dateNaissanceGet();
-    int numeroAdresseGet();
-    string adresseGet();
+    //int numeroAdresseGet();
+    //string adresseGet();
     int telGet();
     int comptecourantGet();
     int compteepargneGet();
@@ -41,9 +41,9 @@ public:
     int numeroClientGet1() const;
     string  nomGet1() const;
     string prenomGet1() const ;
-    int  dateNaissanceGet1() const;
-    int  numeroAdresseGet1() const;
-    string   adresseGet1() const;
+    int dateNaissanceGet1() const ;
+    //int  numeroAdresseGet1() const;
+    //string   adresseGet1() const;
     int  telGet1() const;
     int  comptecourantGet1() const;
     int  compteepargneGet1() const;
@@ -56,8 +56,8 @@ public:
     void nomSet(string value);
     void prenomSet(string value);
     void dateNaissanceSet(int value);
-    void numeroAdresseSet(int value);
-    void adresseSet(string value);
+    //void numeroAdresseSet(int value);
+    //void adresseSet(string value);
     void telSet(int value);
     void comptecourantSet(int value);
     void compteepargneSet(int value);
