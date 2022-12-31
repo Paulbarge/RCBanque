91ea9d542636d6e1c1feb4af45decc52e91ef391
Baptiste LIBERT <baptiste.libert@student.junia.com>
2022-12-31 15:51:14 +0100

    new socket client

--- socket/clientSocket/client.cpp
+++ socket/clientSocket/client.cpp
@@ -4,37 +4,18 @@
 #include "client.h"
 
 
-
-
 #include <iostream>
+#include <string>
 #include <boost/asio.hpp>
 
 
 
-
+using namespace std;
 using namespace boost::asio;
 using ip::tcp;
 using std::string;
 using std::cout;
 using std::endl;
-
-
-class newClient {
-private:
-    string nameClient;
-
-public:
-    void setNameClient(string);
-    string getNameClient();
-};
-
-void newClient::setNameClient(string name) {
-    this->nameClient = name;
-}
-
-string newClient::getNameClient() {
-    return this->nameClient + "\n";
-}
 
 
 
@@ -47,61 +28,31 @@ void verifErreur(boost::system::error_code error, string message) {
     }
 }
 
-void nouveauMsg(string message) {
+void envoiUnique(tcp::socket& socket, string message) {
 
-    boost::asio::io_service io_service;
-    //socket creation
-    tcp::socket socket(io_service);
+
     //connection
     socket.connect(tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 1234));
     boost::system::error_code error;
+    const string msg = message + "\n";
 
-    boost::asio::write(socket, boost::asio::buffer(message), error);
+    boost::asio::write(socket, boost::asio::buffer(msg), error);
+    verifErreur(error, message);
+
+    socket.close(error);
 
 }
 
 
-int main() {
-
-
-    boost::asio::io_service io_service;
-    //socket creation
-    tcp::socket socket(io_service);
+void  envoiReception(tcp::socket& socket, string message) {
     //connection
     socket.connect(tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 1234));
     boost::system::error_code error;
+    const string msg = message + "\n";
 
-
-
-
-
-    const string msg = "Hello from Client!\n";
     boost::asio::write(socket, boost::asio::buffer(msg), error);
-    verifErreur(error, msg);
+    verifErreur(error, message);
 
-
-
-    string msg2 = "Hello c'est moi\n";
-    boost::asio::write(socket, boost::asio::buffer(msg2), error);
-    verifErreur(error, msg2);
-
-
-
-    msg2 = "Hello c'est moi2\n";
-    boost::asio::write(socket, boost::asio::buffer(msg2), error);
-    verifErreur(error, msg2);
-    msg2 = "Hello c'est moi3\n";
-    boost::asio::write(socket, boost::asio::buffer(msg2), error);
-    verifErreur(error, msg2);
-
-
-    msg2 = "Hello c'est moi4\n";
-    boost::asio::write(socket, boost::asio::buffer(msg2), error);
-    verifErreur(error, msg2);
-
-
-
-    // getting response from server
     boost::asio::streambuf receive_buffer;
     boost::asio::read(socket, receive_buffer, boost::asio::transfer_all(), error);
     if (error && error != boost::asio::error::eof) {
@@ -112,14 +63,95 @@ int main() {
         cout << data << endl;
     }
 
-
     socket.close(error);
-    if (error) {
-        cout << "fermeture ok" << endl;
+}
+
+
+void fonctionnementComplet(/*tcp::socket& socket*/) {
+
+    boost::asio::io_service io_service;
+    //socket creation
+    tcp::socket socket(io_service);
+
+    while (true) {
+        string message;
+
+        cout << "Entrez un mot : " << endl;
+        cin >> message;
+
+        envoiReception(socket, message);
     }
-    else {
-        cout << "pb fermeture" << endl;
+}
+
+
+bool momentInterets() {
+    cout << "Hehe les interets" << endl;
+    cout << "timestamp : " << time(NULL) << endl;
+
+    while (true) {
+        cout << "\n#####  Hehe  #####" << endl;
+        Sleep(2000);
+
+
+
     }
+}
+
+
+
+int main() {
+
+
+    //boost::asio::io_service io_service;
+    ////socket creation
+    //tcp::socket socket(io_service);
+    //
+    //envoiUnique(socket, "Test");
+
+
+    std::thread leTraitement(fonctionnementComplet, socket);
+    //std::thread nouveauxInterets(momentInterets);
+
+    leTraitement.join();
+    //nouveauxInterets.join();
+
+
+    fonctionnementComplet();
+
+    while (true) {
+
+
+
+        /*string message;
+
+        cout << "Entrez un mot : " << endl;
+        cin >> message;
+
+        envoiReception(socket, message);
+
+        socket.connect(tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 1234));
+        boost::system::error_code error;
+        const string msg = message + "\n";
+
+        boost::asio::write(socket, boost::asio::buffer(msg), error);
+        verifErreur(error, message);
+
+
+        boost::asio::streambuf receive_buffer;
+        boost::asio::read(socket, receive_buffer, boost::asio::transfer_all(), error);
+        if (error && error != boost::asio::error::eof) {
+            cout << "receive failed: " << error.message() << endl;
+        }
+        else {
+            const char* data = boost::asio::buffer_cast<const char*>(receive_buffer.data());
+            cout << data << endl;
+        }
+
+        socket.close(error);*/
+
+
+    }
+
 
 
 
@@ -127,93 +159,3 @@ int main() {
     getchar();
     return 0;
 }
-
-
-
-
-
-
-
-
-//using namespace std;
-//#include <iostream>
-//#include <boost/asio.hpp>
-//
-//using namespace boost::asio;
-//using ip::tcp;
-//using std::string;
-//using std::cout;
-//using std::endl;
-//
-//
-//class newClient {
-//private:
-//    string nameClient;
-//
-//public:
-//    void setNameClient(string);
-//    string getNameClient();
-//};
-//
-//void newClient::setNameClient(string name) {
-//    this->nameClient = name;
-//}
-//
-//string newClient::getNameClient() {
-//    return this->nameClient+"\n";
-//}
-//
-//
-//int main() {
-//    cout << "### Fenetre client ###\n" << endl;
-//
-//    
-//    newClient client1;
-//
-//    client1.setNameClient("Baptiste");
-//    cout << "Nom client1 : " << client1.getNameClient() << endl;
-//
-//    boost::asio::io_service io_service;
-//    //socket creation
-//    tcp::socket socket(io_service);
-//
-//    tcp::resolver resolver(io_service);
-//    boost::asio::connect(socket, resolver.resolve({ "127.0.0.1", "1234" }));
-//
-//    //connection
-//    //socket.connect(tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 1234));
-//    // request/message from client
-//
-//    boost::system::error_code error;
-//
-//    const string msg = "Hello from Client!\n";
-//    cout << "msg vaut : " << msg << endl;
-//    
-//    boost::asio::write(socket, boost::asio::buffer(msg), error);
-//
-//    /*boost::asio::write(socket, boost::asio::buffer(msg), error);*/
-//
-//    const string msg2 = (string)client1.getNameClient();
-//    cout << "msg2 vaut : " << msg2 << endl;
-//    boost::asio::write(socket, boost::asio::buffer(msg2), error);
-//    
-//    if (!error) {
-//        cout << "Client sent hello message!" << endl;
-//        cout << "Je suis le client" << endl;
-//    }
-//    else {
-//        cout << "send failed: " << error.message() << endl;
-//    }
-//    // getting response from server
-//    boost::asio::streambuf receive_buffer;
-//    boost::asio::read(socket, receive_buffer, boost::asio::transfer_all(), error);
-//    if (error && error != boost::asio::error::eof) {
-//        cout << "receive failed: " << error.message() << endl;
-//    }
-//    else {
-//        const char* data = boost::asio::buffer_cast<const char*>(receive_buffer.data());
-//        cout << data << endl;
-//    }
-//    getchar();
-//    return 0;
-//}
